ce8739c8fcbfe7900a093ec1cde06433f24c10a1
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-12-12 21:19:39 -0800

    chrome and edge support, better build tooling

---
+++ firefox/popup/_next/static/rWHdjXHX2RkL5LQka4rSo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/544-62709aeacd199ddd.js","static/chunks/pages/index-c6385b3d335cc3ed.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- popup-ui/utils/storage.js
+++ popup-ui/utils/storage.js
@@ -1,17 +1,42 @@
-/* global browser */
+/* global browser, chrome */
 
-const hasBrowser = typeof browser !== 'undefined' 
+const hasBrowser = typeof browser !== 'undefined'
+const hasChrome = typeof chrome !== 'undefined'
 
 export const setStorage = (data) => {
   if (!hasBrowser) {
-    return Promise.resolve({})
+    if (hasChrome) {
+      return new Promise((resolve, reject) => {
+        chrome.storage.local.set({ data }, () => {
+          if (chrome.runtime.lastError) {
+            reject(chrome.runtime.lastError)
+          } else {
+            resolve()
+          }
+        })
+      })
+    } else {
+      return Promise.resolve({})
+    }
   }
   return browser.storage.local.set({ data })
 }
 
 export const getStorage = () => {
   if (!hasBrowser) {
-    return Promise.resolve({})
+    if (hasChrome) {
+      return new Promise((resolve, reject) => {
+        chrome.storage.local.get('data', data => {
+          if (chrome.runtime.lastError) {
+            reject(chrome.runtime.lastError)
+          } else {
+            resolve(data)
+          }
+        })
+      })
+    } else {
+      return Promise.resolve({})
+    }
   }
   return browser.storage.local.get('data').then(data => {
     return data

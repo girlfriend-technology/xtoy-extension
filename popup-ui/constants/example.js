cd23d373fba4b0a4941d46d0eea01837924d2590
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-07-05 21:59:21 -0700

    better examples

--- popup-ui/constants/example.js
+++ popup-ui/constants/example.js
@@ -1,32 +1,74 @@
 const example = [
   {
     enabled: false,
-    id: 'example-1',
+    id: "example-1",
     links: false,
-    x: 'hack',
-    y: 'work'
+    x: "hack",
+    y: "work",
   },
   {
     enabled: false,
-    id: 'example-2',
+    id: "example-2",
     links: false,
-    x: 'MyDeadname',
-    y: 'MyNewname'
+    x: "MyDeadname",
+    y: "MyNewname",
   },
   {
     enabled: false,
-    id: 'example-3',
+    id: "example-3",
     links: false,
-    x: '/\\d+/g',
-    y: '69'
+    x: "/\\d[\\d,.]*/g",
+    y: "69",
   },
   {
     enabled: false,
-    id: 'example-4',
+    id: "example-4",
     links: true,
-    x: 'https://twitter.com/',
-    y: 'https://nitter.ir/'
-  }
-]
+    x: "/https?://(www.)*twitter.com/im",
+    y: "https://nitter.net",
+  },
+  {
+    enabled: false,
+    id: "example-5",
+    links: true,
+    x: "/https?://www.amazon.com/im",
+    y: "https://smile.amazon.com",
+  },
+  {
+    enabled: false,
+    id: "example-6",
+    links: true,
+    x: "/https?://(www.)*youtube.com/im",
+    y: "https://invidious.weblibre.org",
+  },
+  {
+    enabled: false,
+    id: "example-7",
+    links: true,
+    x: "/https?://(www.)*instagram.com/p//im",
+    y: "https://bibliogram.art/p/",
+  },
+  {
+    enabled: false,
+    id: "example-8",
+    links: true,
+    x: "/https?://(www.)*instagram.com/im",
+    y: "https://bibliogram.art/u",
+  },
+  {
+    enabled: false,
+    id: "example-9",
+    links: true,
+    x: "/https?://(www.)*reddit.com/im",
+    y: "https://libreddit.spike.codes",
+  },
+  {
+    enabled: false,
+    id: "example-10",
+    links: true,
+    x: "/https?://(www.)*tiktok.com/im",
+    y: "https://proxitok.herokuapp.com",
+  },
+];
 
-export default example
+export default example;
